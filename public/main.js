b46678e588aa8f075e4d9a348a9372fedfe50fcb
calebfergie <yonohabloespanol@gmail.com>
2020-11-08 13:39:31 -0500

    update urls

--- public/main.js
+++ public/main.js
@@ -1,13 +1,7 @@
-// Telling server when new user is connected
 
-// socket.on('userCount', function(userCount) {
-//   console.log('total number of users online is: ' + userCount); // console number of users after one goes off;
-// });
-//
-// function alertFunc(){
-// console.log("somebody disconnected");
-// };
-// // On disconnect
-// socket.on('disconnect', function(){
-//   alertFunc();
-// });
+document.getElementById("github-button").onclick = function() {sendStatus()};
+
+function sendStatus() {
+  console.log("sending status")
+
+};
